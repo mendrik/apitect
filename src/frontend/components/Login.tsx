b724b420a521cd4428caeeb10febaf00eb3315c4
Andreas Herd <mendrik76@gmail.com>
2021-09-25 11:05:24 +0300

    layout login

--- src/frontend/components/Login.tsx
+++ src/frontend/components/Login.tsx
@@ -6,11 +6,13 @@ import { LoginForm } from './LoginForm'
 
 export const Login: FC = () => {
   return (
-    <Tabs>
-      <Tab title="app.login">
-        <LoginForm />
-      </Tab>
-      <Tab title="app.register">B</Tab>
-    </Tabs>
+    <div className="d-flex flex-column justify-content-md-center align-items-center vh-100 p-4">
+      <Tabs className="min-w-modal">
+        <Tab title="app.login">
+          <LoginForm />
+        </Tab>
+        <Tab title="app.register">B</Tab>
+      </Tabs>
+    </div>
   )
 }
--- src/frontend/forms/Tabs.tsx
+++ src/frontend/forms/Tabs.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx'
-import React, { Children, createContext, FC, ReactElement } from 'react'
+import React, { Children, createContext, FC, HTMLAttributes, ReactElement } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { OwnProps as TabProps } from './Tab'
@@ -13,26 +13,28 @@ const tabContext = createContext<TabContext>({ activeTab: 0 })
 type OwnProps = {
   activeTab?: number
   children: ReadonlyArray<ReactElement<TabProps>>
-}
+} & HTMLAttributes<HTMLDivElement>
 
-export const Tabs: FC<OwnProps> = ({ activeTab = 0, children }) => {
+export const Tabs: FC<OwnProps> = ({ activeTab = 0, children, ...props }) => {
   const { t } = useTranslation()
   return (
-    <tabContext.Provider value={{ activeTab }}>
-      <ul className="nav nav-tabs">
-        {Children.map(children, (child, i) => (
-          <li className="nav-item">
-            <button
-              type="button"
-              className={clsx('nav-link', { active: i === activeTab })}
-              aria-current="page"
-            >
-              {t(child.props.title)}
-            </button>
-          </li>
-        ))}
-      </ul>
-      {children[activeTab]}
-    </tabContext.Provider>
+    <div {...props}>
+      <tabContext.Provider value={{ activeTab }}>
+        <ul className="nav nav-tabs mb-4">
+          {Children.map(children, (child, i) => (
+            <li className="nav-item">
+              <button
+                type="button"
+                className={clsx('nav-link', { active: i === activeTab })}
+                aria-current="page"
+              >
+                {t(child.props.title)}
+              </button>
+            </li>
+          ))}
+        </ul>
+        {children[activeTab]}
+      </tabContext.Provider>
+    </div>
   )
 }
