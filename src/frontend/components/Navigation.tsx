9c125f8fa567bfe2b4a2b3ce05f52287a951a560
Andreas Herd <mendrik76@gmail.com>
2021-09-29 14:23:13 +0300

    fix nav

--- src/frontend/components/Navigation.tsx
+++ src/frontend/components/Navigation.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react'
-import { Button, NavDropdown } from 'react-bootstrap'
+import { Button, Nav, Navbar, NavDropdown } from 'react-bootstrap'
 import { LogIn } from 'react-feather'
 import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router-dom'
@@ -13,28 +13,29 @@ export const Navigation: FC = () => {
   const navigate = useNavigate()
   const [userState, logout] = useLogout()
   return (
-    <nav className="navbar navbar-light px-2 bg-light shadow-sm flex flex-row justify-content-start align-items-center">
-      <div className="flex-grow-1">
-        <button type="button" className="navbar-brand btn">
-          <Logo className="logo" style={{ height: 32 }} />
-        </button>
-      </div>
-      <div>
-        {userState.data != null ? (
-          <NavDropdown id="nav-dropdown-dark-example" title={userState.data.name}>
-            <NavDropdown.Item>Profile</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item onClick={logout}>{t('navbar.logout')}</NavDropdown.Item>
-          </NavDropdown>
-        ) : (
-          <Button onClick={() => navigate(addParams({ modal: 'login' }))} className="ps-2 d-flex">
-            <div className="d-flex gap-1 align-items-center">
-              <LogIn className="icon-xs ms-1" />
-              <span>{t('navbar.login')}</span>
-            </div>
-          </Button>
-        )}
-      </div>
-    </nav>
+    <Navbar variant="light" expand="md" className="px-2 shadow-lg">
+      <Navbar.Brand href="#home">
+        <Logo className="logo" style={{ height: 32, marginTop: -5 }} />
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-end">
+        <Nav className="ml-auto">
+          {userState.data != null ? (
+            <NavDropdown title={userState.data.name} align="end">
+              <NavDropdown.Item>Profile</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item onClick={logout}>{t('navbar.logout')}</NavDropdown.Item>
+            </NavDropdown>
+          ) : (
+            <Button onClick={() => navigate(addParams({ modal: 'login' }))} className="ps-2 d-flex">
+              <div className="d-flex gap-1 align-items-center">
+                <LogIn className="icon-xs ms-1" />
+                <span>{t('navbar.login')}</span>
+              </div>
+            </Button>
+          )}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
