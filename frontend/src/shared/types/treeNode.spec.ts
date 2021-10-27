dbdc21e81eccd0b905b89de27530ab6af3a8479a
Andreas Herd <mendrik76@gmail.com>
2021-10-27 19:55:10 +0300

    tree struct

---
+++ frontend/src/shared/types/treeNode.spec.ts
@@ -0,0 +1,30 @@
+import { Strategy, TreeNode } from './treeNode'
+
+describe('TreeNode', () => {
+  it('Flatten depth first', () => {
+    const x = TreeNode.of(1, [
+      TreeNode.of(2),
+      TreeNode.of(3, [TreeNode.of(4, [TreeNode.of(5)]), TreeNode.of(6)]),
+      TreeNode.of(7)
+    ])
+    expect(x.flatten(Strategy.Depth)).toStrictEqual([1, 2, 3, 4, 5, 6, 7])
+  })
+
+  it('Flatten breadth first', () => {
+    const x = TreeNode.of(1, [
+      TreeNode.of(2),
+      TreeNode.of(3, [TreeNode.of(5, [TreeNode.of(7)])]),
+      TreeNode.of(4, [TreeNode.of(6)])
+    ])
+    expect(x.flatten()).toStrictEqual([1, 2, 3, 4, 5, 6, 7])
+  })
+
+  it('Map tree', () => {
+    const x = TreeNode.of(1, [
+      TreeNode.of(2),
+      TreeNode.of(3, [TreeNode.of(5, [TreeNode.of(7)])]),
+      TreeNode.of(4, [TreeNode.of(6)])
+    ])
+    expect(x.map(n => `${n}`).flatten()).toStrictEqual(['1', '2', '3', '4', '5', '6', '7'])
+  })
+})
--- frontend/src/shared/types/treeNode.ts
+++ frontend/src/shared/types/treeNode.ts
@@ -1,33 +1,44 @@
 import fl from 'fantasy-land'
-import { apply, eqBy, equals, invoker, map, prop, propOr } from 'ramda'
+import { map } from 'ramda'
 
-type TreeNodeValue = {
-  [fl.equals]: (that: TreeNodeValue) => boolean
-  [fl.lte]: (that: TreeNodeValue) => boolean
+export enum Strategy {
+  Depth,
+  Breadth
 }
 
-type Func<T extends TreeNodeValue> = (x: T) => T
+export class Queue<T> extends Array<T> {
+  pop(): T | undefined {
+    return super.shift()
+  }
 
-export class TreeNode<T extends TreeNodeValue> {
+  push(...items: T[]): number {
+    return super.push(...items.reverse())
+  }
+}
+
+export class TreeNode<T> {
   constructor(public value: T, public children: TreeNode<T>[]) {}
 
-  [fl.equals] = ({ value, children }: TreeNode<T>): boolean =>
-    equals(this.value, value) &&
-    eqBy(propOr(0, 'length')) &&
-    this.children.every((child: TreeNode<T>, i: number) => child[fl.equals](children[i]));
+  [fl.map] = <R>(fn: (v: T) => R): TreeNode<R> =>
+    new TreeNode<R>(
+      fn(this.value),
+      map(c => c[fl.map](fn), this.children)
+    )
+  map = this[fl.map]
 
-  [fl.lte] = ({ value, children }: TreeNode<T>): boolean =>
-    this.value[fl.lte](value)
-      ? this.value[fl.equals](value)
-        ? this.children[fl.lte](children)
-        : true
-      : false;
+  static [fl.of] = <ST>(value: ST, children: TreeNode<ST>[] = []) =>
+    new TreeNode<ST>(value, children)
+  static of = TreeNode[fl.of]
 
-  [fl.map] = (f: Func<T>) => new TreeNode(f(this.value), map(apply(fl.map), this.children.map));
-
-  [fl.reduce] = (f: Func<T>, acc) =>
-    this.children.reduce((acc, rt) => rt[fl.reduce](f, acc), f(acc, this.value));
-
-  [fl.traverse] = (T, f: Func<T>) =>
-    this.children[fl.traverse](T, f)[fl.ap](f(this.value).map(v => cs => new TreeNode(v, cs)))
+  flatten = (strategy: Strategy = Strategy.Breadth): T[] => {
+    const arr =
+      strategy === Strategy.Depth ? Array.of<TreeNode<T>>(this) : Queue.of<TreeNode<T>>(this)
+    const res = Array.of<T>()
+    while (arr.length > 0) {
+      const el = arr.pop()!
+      res.push(el.value)
+      arr.push(...el.children.reverse())
+    }
+    return res
+  }
 }
