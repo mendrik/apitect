a24829b12ba1ee69aaa809c5c2eeb3b89c89fbee
Andreas Herd <mendrik76@gmail.com>
2021-11-30 16:30:48 +0200

    add renaming

--- server/src/services/tags.ts
+++ server/src/services/tags.ts
@@ -1,8 +1,14 @@
 import { logger } from '~shared/utils/logger'
 
+import { collection, Collections } from './database'
+
 export const renameTag =
   (docId: string) =>
-  (from: string, to: string): Promise<void> => {
+  (from: string, to: string): Promise<any> => {
     logger.info(`Renaming tag ${from} into ${to} in doc ${docId}`)
-    return Promise.resolve()
+    return collection(Collections.userSettings).updateMany(
+      { docId },
+      { $set: { 'visibleTags.$[filter]': to } },
+      { arrayFilters: [{ filter: from }] }
+    )
   }
