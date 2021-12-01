868811db7d34d00254da4ce3fbab7386536b3b84
Andreas Herd <mendrik76@gmail.com>
2021-12-01 10:43:11 +0200

    fix regexp

--- frontend/src/shared/codecs/nameCodec.ts
+++ frontend/src/shared/codecs/nameCodec.ts
@@ -2,8 +2,8 @@ import { string } from 'zod'
 
 export const nameCodec = string()
   .nonempty('form.validation.required')
-  .regex(/^[a-zA-Z0-1-$]+$/i, 'form.validation.noSpecialCharactersOrSpace')
+  .regex(/^[a-zA-Z0-9-$]+$/i, 'form.validation.noSpecialCharactersOrSpace')
 
 export const nameCodecWithSpaces = string()
   .nonempty('form.validation.required')
-  .regex(/^[a-zA-Z0-1-$ ]+$/i, 'form.validation.noSpecialCharacters')
+  .regex(/^[a-zA-Z0-9-$ ]+$/i, 'form.validation.noSpecialCharacters')
