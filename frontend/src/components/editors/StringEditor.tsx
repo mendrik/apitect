94d6f4ab51bbc599e7d09ecf29cb06a03f02d353
Andreas Herd <mendrik76@gmail.com>
2021-12-03 15:07:53 +0200

    fix imports

--- frontend/src/components/editors/StringEditor.tsx
+++ frontend/src/components/editors/StringEditor.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { identity, juxt } from 'ramda'
+import React, { KeyboardEvent } from 'react'
 
 import { Jsx } from '../../shared/types/generic'
 import { EditorProps } from '../specific/VisualValue'
 
-export const StringEditor = ({ node, settings, value, children, ...props }: Jsx<EditorProps>) => (
-  <input type="text" className="editor" autoFocus placeholder={node.name} {...props} />
-)
+export const StringEditor = ({
+  node,
+  settings,
+  value,
+  children,
+  onKeyDown = identity,
+  ...props
+}: Jsx<EditorProps>) => {
+  const test = (ev: KeyboardEvent) => {
+    console.log(ev)
+  }
+  return (
+    <input
+      type="text"
+      className="editor"
+      autoFocus
+      placeholder={node.name}
+      onKeyDown={juxt([onKeyDown, test])}
+      {...props}
+    />
+  )
+}
