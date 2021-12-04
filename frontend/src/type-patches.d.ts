0b3cbf9b9bc475387ed31d496daacfeddd86868d
Andreas Herd <mendrik76@gmail.com>
2021-12-04 21:27:01 +0200

    fix build with vite

--- frontend/src/type-patches.d.ts
+++ frontend/src/type-patches.d.ts
@@ -1,7 +1,7 @@
-declare global {
-  declare module 'react-i18next' {
-    export interface Resources {
-      readonly translation: typeof import('./locales/apitect.en-GB').default.translation
-    }
+export {}
+
+declare module 'react-i18next' {
+  export interface Resources {
+    readonly translation: typeof import('./locales/apitect.en-GB').default.translation
   }
 }
