657a7f87cedfd5ae80d2af6bace7740bfba6e95b
Andreas Herd <mendrik76@gmail.com>
2021-12-05 11:49:11 +0200

    tweak user settings

--- frontend/src/stores/$userStore.ts
+++ frontend/src/stores/$userStore.ts
@@ -1,10 +1,7 @@
 import { createStore } from 'effector'
 import { User } from '~shared/types/domain/user'
-import { logger } from '~shared/utils/logger'
 
 import { whoAmIFx } from '../events/user'
 
 export const $user = createStore<User | null>(null)
 $user.on(whoAmIFx.done, (_, { result }) => result)
-
-whoAmIFx.watch(() => logger.info('user loaded'))
