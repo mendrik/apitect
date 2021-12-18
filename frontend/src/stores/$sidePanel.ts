f9b080ec735afae1d78b3723016f737e2623160e
Andreas Herd <mendrik76@gmail.com>
2021-12-18 21:09:52 +0200

    add sidepanel logic

--- frontend/src/stores/$sidePanel.ts
+++ frontend/src/stores/$sidePanel.ts
@@ -1,13 +1,20 @@
 import { createStore, sample } from 'effector'
-import { flip, map, pipe, prop, without } from 'ramda'
+import { concat, flip, map, pipe, prop, without } from 'ramda'
 import { propNotEq } from 'ramda-adjunct'
 import { Node } from '~shared/types/domain/node'
 import { NodeType } from '~shared/types/domain/nodeType'
+import { $selectedValueNode } from '~stores/$valuesStore'
 
 export const $sidePanelOpen = createStore(false)
 export const $selectedNodePath = createStore<Node[]>([])
 
 const openFor = [NodeType.Array, NodeType.Date, NodeType.Binary]
+
+sample({
+  source: $selectedValueNode,
+  fn: node => (node != null ? concat([node.extract()], node.pathToRoot()) : []),
+  target: $selectedNodePath
+})
 
 sample({
   source: $selectedNodePath,
