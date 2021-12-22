bfa57d67372fe129cd609f7b7bf6bec1630e2f48
Andreas Herd <mendrik76@gmail.com>
2021-12-22 14:16:27 +0200

    fix toJson

--- frontend/src/shared/utils/nodeToJson.ts
+++ frontend/src/shared/utils/nodeToJson.ts
@@ -1,5 +1,4 @@
-import { cond, propEq, T as Otherwise } from 'ramda'
-import { isNilOrEmpty } from 'ramda-adjunct'
+import { cond, pipe, propEq, propOr, reduce, T as Otherwise } from 'ramda'
 import { TreeNode } from '~shared/algebraic/treeNode'
 import { Node, NodeId } from '~shared/types/domain/node'
 import { NodeType } from '~shared/types/domain/nodeType'
@@ -8,17 +7,17 @@ import { Value } from '~shared/types/domain/values/value'
 export const nodeToJson = (node: TreeNode<Node>, values: Record<NodeId, Value>) => {
   const getArrayContent = (child: Node) => []
 
-  const childJson: (node: Node) => object = cond([
+  const toJson: (node: Node) => object = cond([
     [
       propEq('nodeType', NodeType.Object),
-      (node: Node) =>
-        isNilOrEmpty(node.children)
-          ? values[node.id]?.value
-          : node.children.reduce((acc, cur) => ({ ...acc, [cur.name]: childJson(cur) }), {})
+      pipe<any, Node[], any>(
+        propOr([], 'children'),
+        reduce((acc, cur) => ({ ...acc, [cur.name]: toJson(cur) }), {})
+      )
     ],
     [propEq('nodeType', NodeType.Array), getArrayContent],
     [Otherwise, (node: Node) => values[node.id]?.value]
   ])
 
-  return node.map(childJson).extract()
+  return toJson(node.extract())
 }
