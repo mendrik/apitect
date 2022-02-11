5503ea8f514494922238b8b24bcc03a4b880fc78
Andreas Herd <mendrik76@gmail.com>
2022-02-11 11:27:28 +0200

    code improvements

--- frontend/src/components/forms/SocketForm.tsx
+++ frontend/src/components/forms/SocketForm.tsx
@@ -1,10 +1,10 @@
 import { EffectByHandler } from 'effector'
-import { useStore } from 'effector-react'
-import React, { PropsWithChildren, ReactElement, ReactNode, useEffect, useState } from 'react'
+import React, { PropsWithChildren, ReactElement, ReactNode, useState } from 'react'
 import { Button } from 'react-bootstrap'
 import { FormProvider, UseFormReturn } from 'react-hook-form'
+import { FieldValues } from 'react-hook-form/dist/types/fields'
 import { TFuncKey, useTranslation } from 'react-i18next'
-import type { Api, FormApiMethod } from '~shared/apiTypes'
+import { Api, FormApiMethod } from '~shared/apiTypes'
 import { Fn } from '~shared/types/generic'
 
 import { Scale, Tuple } from '../generic/Tuple'
@@ -12,40 +12,41 @@ import { ButtonRow } from './ButtonRow'
 import { GenericError, isExtendedError } from './GenericError'
 import { SubmitButton } from './SubmitButton'
 
-type OwnProps<M extends FormApiMethod> = {
-  form: UseFormReturn<any>
-  onValid: EffectByHandler<Api[M], Error>
+type OwnProps<M extends FormApiMethod, F extends FieldValues> = {
+  form: UseFormReturn<F>
+  onValid: (arg: F) => ReturnType<EffectByHandler<Api[M], Error>>
   close: Fn
   submitButton: TFuncKey
   buttonRowExtras?: ReactNode
 }
 
-export const SocketForm = <M extends FormApiMethod>({
+export const SocketForm = <M extends FormApiMethod, F extends FieldValues>({
   form,
   children,
   onValid,
   submitButton,
   buttonRowExtras,
   close
-}: PropsWithChildren<OwnProps<M>>): ReactElement | null => {
+}: PropsWithChildren<OwnProps<M, F>>): ReactElement | null => {
   const [error, setError] = useState<Error | null>(null)
-  const running = useStore(onValid.pending)
-
-  useEffect(() => {
-    const sub = onValid.failData.watch(e => {
-      if (isExtendedError(e) && e.field != null) {
-        return form.setError(e.field as any, { message: e.message })
-      }
-      setError(e)
-    })
-    return () => sub.unsubscribe()
-  }, [form, setError, onValid])
+  const [running, setRunning] = useState(false)
 
   const { t } = useTranslation()
   return (
     <FormProvider {...form}>
       <form
-        onSubmit={form.handleSubmit(data => onValid(data).then(close))}
+        onSubmit={form.handleSubmit(data => {
+          setRunning(true)
+          return onValid(data as F)
+            .then(close)
+            .catch(e => {
+              if (isExtendedError(e) && e.field != null) {
+                return form.setError(e.field as any, { message: e.message })
+              }
+              setError(e)
+            })
+            .finally(() => setRunning(false))
+        })}
         data-disabled={running ? 'true' : 'false'}
         noValidate
       >
