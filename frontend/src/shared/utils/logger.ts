4b5bb202d7bc8bfece0d0654f21c0b4b30a15232
Andreas Herd <mendrik76@gmail.com>
2022-09-19 23:10:27 +0300

    fix colors

--- frontend/src/shared/utils/logger.ts
+++ frontend/src/shared/utils/logger.ts
@@ -1,17 +1,17 @@
-import { blue, cyan, darkGray, lightGray, red } from 'ansicolor'
+import { blue, cyan, darkGray, lightGray, red, yellow } from 'ansicolor'
 import { log } from 'shrink-json'
 
 const state = (state: string, color = cyan) => lightGray(`[${color(state.toUpperCase())}] `)
 export const logger = {
   log: log(),
   info: (message: string, obj?: any) =>
-    console.log(state('INFO') + blue(`${message}${obj ? ` ${logger.log(obj)}` : ''}`)),
+    console.log(state('INFO') + lightGray(`${message}${obj ? ` ${logger.log(obj)}` : ''}`)),
   message: (message: string, obj?: any) =>
-    console.log(state('MSG', blue) + blue(`${message}${obj ? ` ${logger.log(obj)}` : ''}`)),
+    console.log(state('MSG', blue) + lightGray(`${message}${obj ? ` ${logger.log(obj)}` : ''}`)),
   error: (message: string, obj?: any) =>
     console.log(state('ERROR', red) + red(`${message}${obj ? ` ${logger.log(obj)}` : ''}`)),
   warn: (message: string, obj?: any) =>
-    console.warn(state('WARN') + cyan(`${message}${obj ? ` ${logger.log(obj)}` : ''}`)),
+    console.warn(state('WARN') + yellow(`${message}${obj ? ` ${logger.log(obj)}` : ''}`)),
   debug: (message: string, obj?: any) =>
     console.debug(state('DEBUG') + darkGray(`${message}${obj ? ` ${logger.log(obj)}` : ''}`))
 }
