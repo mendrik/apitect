bca775d57d7444a0ee7d35dd3a72724a37b4d2dd
Andreas Herd <mendrik76@gmail.com>
2022-09-22 19:33:21 +0300

    remove unused code

--- frontend/src/stores/$arrayItemsStore.ts
+++ frontend/src/stores/$arrayItemsStore.ts
@@ -1,11 +1,9 @@
 import { createStore, sample } from 'effector'
 import { apply } from 'ramda'
 import { isNotNil } from 'ramda-adjunct'
-import { Api } from '~shared/apiTypes'
 import { Id } from '~shared/types/domain/id'
 import { Json } from '~shared/types/generic'
 import { matches } from '~shared/utils/ramda'
-import { $api } from '~stores/$apiStore'
 import { $selectedArrayNode } from '~stores/$arrayStores'
 import { $currentTag } from '~stores/$currentTag'
 
@@ -15,8 +13,6 @@ type Item = Json & { id: Id }
 
 export const $arrayItems = createStore<Item[]>([])
 export const $selectedArrayItem = createStore<Item | null>(null)
-
-const api = (): Api => sample({ source: $api }).getState()
 
 sample({
   source: [$currentTag, $selectedArrayNode] as [typeof $currentTag, typeof $selectedArrayNode],
