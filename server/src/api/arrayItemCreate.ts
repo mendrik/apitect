874434accbfda29728c875313b65c6477055f254
Andreas Herd <mendrik76@gmail.com>
2022-09-23 20:47:16 +0300

    clean up datasource code

--- server/src/api/arrayItemCreate.ts
+++ server/src/api/arrayItemCreate.ts
@@ -1,15 +1,10 @@
-import { cond, propEq } from 'ramda'
 import { ServerApiMethod } from '~shared/apiResponse'
 import { Value } from '~shared/types/domain/values/value'
-import { ArraySettings, DataSourceType } from '~shared/types/forms/nodetypes/arraySettings'
 import { CreateItemResponse } from '~shared/types/response/createItemResponse'
 import { resolvePromised } from '~shared/utils/promise'
 
 import { validateValues } from '../datasources/arrayItem'
-import { DataSource } from '../datasources/datasource'
-import { externalDataSource } from '../datasources/externalDatabase'
-import { internalDataSource } from '../datasources/internal'
-import { nodeSettings } from './nodeSettings'
+import { getDataSource } from '../services/datasource'
 
 /**
  * The item is created from existing values that have no arrayNodeId assigned to them
@@ -25,17 +20,7 @@ export const arrayItemCreate: ServerApiMethod<'arrayItemCreate'> = async ({
   payload: { arrayNodeId, tag }
 }) => {
   const values: Value[] = await validateValues(docId, email, tag, arrayNodeId)
-  const arraySettings = await nodeSettings({ docId, email, payload: arrayNodeId })
-  const dataSource: DataSource = cond([
-    [
-      propEq('dataSource', DataSourceType.Internal),
-      internalDataSource(arrayNodeId, docId, email, tag)
-    ],
-    [
-      propEq('dataSource', DataSourceType.Database),
-      externalDataSource(arrayNodeId, docId, email, tag)
-    ]
-  ])(arraySettings as ArraySettings)
+  const dataSource = await getDataSource(docId, tag, email, arrayNodeId)
   return resolvePromised<CreateItemResponse>({
     arrayItemId: dataSource.insertItem(values),
     arrayNodeId,
--- server/src/datasources/datasource.ts
+++ server/src/datasources/datasource.ts
@@ -1,17 +1,12 @@
 import { Id } from '~shared/types/domain/id'
+import { Item } from '~shared/types/domain/item'
 import { NodeId } from '~shared/types/domain/node'
 import { Value } from '~shared/types/domain/values/value'
 import { ValueList } from '~shared/types/response/valueList'
-
-export type ListResult<T> = {
-  count: number
-  index: number
-  items: T[]
-}
 
 export type DataSource = {
   insertItem: (values: Value[]) => Promise<Id>
   updateItem: (id: Id, values: Record<NodeId, Value>) => Promise<ValueList>
   deleteItem: (id: Id) => Promise<void>
-  list: <T>(index: number, count: number, searchKey?: string) => Promise<ListResult<T>>
+  list: (page: number, pageSize: number, search: Document) => Promise<Item[]>
 }
