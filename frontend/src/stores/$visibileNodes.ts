420b064f5712684e7d740f9a85cd19e0a2c8c5ff
Andreas Herd <mendrik76@gmail.com>
2022-10-02 22:55:37 +0300

    dash (wip)

--- frontend/src/stores/$visibileNodes.ts
+++ frontend/src/stores/$visibileNodes.ts
@@ -1,8 +1,8 @@
 import { combine, Store } from 'effector'
-import { prop } from 'ramda'
 import { isTrue } from 'ramda-adjunct'
 import { Strategy, TreeNode } from '~shared/algebraic/treeNode'
 import { Node, NodeId } from '~shared/types/domain/node'
+import { _ } from '~shared/utils/dash'
 
 import { $openNodes } from './$openNodesStore'
 import { $treeStore } from './$treeStore'
@@ -19,5 +19,6 @@ export const $visibleNodes: Store<NodeId[]> = combine($openNodes, $treeStore, (o
     .filter(isVisible)
     .slice(1) // skip root
     .map((n: TreeNode<Node>) => n.extract())
-    .map(prop('id'))
+    .map(_.id)
+    .map(x => )
 })
