b730daf47af09666d3b281ce9670537fa9ce92d4
Andreas Herd <mendrik76@gmail.com>
2022-10-17 23:26:35 +0300

    prepare lens dropping

--- frontend/src/components/specific/utils/snapToNode.ts
+++ frontend/src/components/specific/utils/snapToNode.ts
@@ -1,5 +1,5 @@
 import { Modifier } from '@dnd-kit/core'
-import { clamp, pathEq, Pred, view } from 'ramda'
+import { clamp, min, pathEq, Pred, view } from 'ramda'
 import { TreeNode } from '~shared/algebraic/treeNode'
 import { Node, NodeId } from '~shared/types/domain/node'
 import { NodeType } from '~shared/types/domain/nodeType'
@@ -17,9 +17,10 @@ export const snapToNode =
     if (!overNode || !activeNode) {
       return transform
     }
+    const isOpen = openNodes[overNode.value.id] ?? false
     const byOverId: Pred<[TreeNode<Node>]> = pathEq(['value', 'id'], overNode.value.id)
-    const depth = overNode.depth + (overNode.value.nodeType === NodeType.Object ? 1 : 0)
-    const nextDepth = view(lensNext(byOverId), visibleNodes)?.depth ?? depth
+    const depth = overNode.depth + (isOpen || overNode.value.nodeType === NodeType.Object ? 1 : 0)
+    const nextDepth = min(view(lensNext(byOverId), visibleNodes)?.depth ?? depth, depth)
     const clampedX = clamp(nextDepth, depth, (transform.x / treeIndent) | 0)
     return {
       ...transform,
